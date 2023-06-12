d878e6fb322d0d05ccc6dac0f0f0f783f98bc72f
Vishal K Saini <sainivk0003@gmail.com>
2023-06-12 20:01:09 +0530

    a

---
+++ src/contant.ts
@@ -0,0 +1,1 @@
+export const MONGO_URL = 'mongodb+srv://Vishal:Vishal@nodejs.pmv9mdd.mongodb.net/?retryWrites=true&w=majority'
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+//import {MONGO_URL} from './contant'
+const app = express();
+app.use(cors({
+    credentials:true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8000,() => {
+    console.log('Server running on port : http://localhost:8000/');
+})
+
+ const MONGO_URL = 'mongodb+srv://Vishal:Vishal@nodejs.pmv9mdd.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise =  Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error:Error) => console.log('DB connection error'));
